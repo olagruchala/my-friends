30c37facf16e4f0a6853e1a1f746a0c4bc2c8b79
Aleksandra Gruchała <gruchalaola@gmail.com>
2019-06-13 13:28:54 +0200

    Updates

---
+++ precache-manifest.f59e4dc7bfe8fadaa5dd15fb67af67e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070ef68a63911221806285cafd4a6692",
+    "url": "/my-friends/index.html"
+  },
+  {
+    "revision": "afd7126fb7799ce562ef",
+    "url": "/my-friends/static/css/main.bcb070a0.chunk.css"
+  },
+  {
+    "revision": "fe358c9901d73c2b4ea4",
+    "url": "/my-friends/static/js/2.45f3aaf2.chunk.js"
+  },
+  {
+    "revision": "afd7126fb7799ce562ef",
+    "url": "/my-friends/static/js/main.bf09dc0e.chunk.js"
+  },
+  {
+    "revision": "a8de3abf44860b9658e8",
+    "url": "/my-friends/static/js/runtime~main.e82ab982.js"
+  },
+  {
+    "revision": "8a5da5cb04b6d86a8675a01bdd74c4f1",
+    "url": "/my-friends/static/media/logo.8a5da5cb.png"
+  }
+]);
