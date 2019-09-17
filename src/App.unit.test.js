04d375e0c8756ed941f8b17da57ac7606a1bc73a
Aleksandra Gruchała <gruchalaola@gmail.com>
2019-09-17 21:19:50 +0200

    Add more unit tests for App component

--- src/App.unit.test.js
+++ src/App.unit.test.js
@@ -7,8 +7,7 @@ import StatusCreate from "./StatusCreate";
 describe('App component', () => {
   let appWrapper;
   let appInstance;
-  const app = () =>
-      shallow(<App />);
+  const app = () => shallow(<App />);
 
   beforeEach(() => {
     appWrapper = app();
@@ -26,7 +25,7 @@ describe('App component', () => {
   });
 
   it('renders a div', () => {
-    expect(shallow(<App />).first().type())
+    expect(app().first().type())
         .toBe('div');
   });
 
@@ -35,10 +34,37 @@ describe('App component', () => {
         .toBe(1);
   });
 
+  describe('the rendered <Header />', () => {
+    const header = () => appWrapper.find(Header);
+
+    it('receives this.state.user.name as a "name" prop', () => {
+      expect(header().prop('name')).toEqual(appWrapper.state('user').name);
+    });
+
+  });
+
   it('renders <StatusCreate />', () => {
     expect(appWrapper.find(StatusCreate).length)
         .toBe(1);
   });
+
+  describe('the rendered <StatusCreate />', () => {
+    const statusCreate = () => appWrapper.find(StatusCreate);
+
+    it('allows for a maximum of 300 characters', () => {
+      expect(statusCreate().prop('maxLetters')).toEqual(300);
+    });
+
+    it('receives this.state.user.name as a "name" prop', () => {
+      expect(statusCreate().prop('name')).toEqual(appWrapper.state('user').name);
+    });
+
+    it('receives this.state.user.email as a "email" prop', () => {
+      expect(statusCreate().prop('name')).toEqual(appWrapper.state('user').email);
+    });
+
+  });
+
 });
 
 
